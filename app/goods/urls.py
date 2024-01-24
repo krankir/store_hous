d03107141a3fc095664a3dfec0455236085e9ecc
Анатолий Редько <redko_anatoly@cybersec74.ru>
2024-01-24 11:29:21 +0500

    second pagination option - GET params

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -6,6 +6,5 @@ app_name = 'goods'
 
 urlpatterns = [
     path('<slug:category_slug>/', catalog, name='index'),
-    path('<slug:category_slug>/<int:page>/', catalog, name='index'),
     path('product/<slug:product_slug>/', product, name='product'),
 ]
