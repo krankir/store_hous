537b8200bf2904abbbc57f91597fce4b3b7d90ca
Анатолий Редько <redko_anatoly@cybersec74.ru>
2024-01-24 14:24:30 +0500

    simple search created

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -4,7 +4,10 @@ from goods.views import catalog, product
 
 app_name = 'goods'
 
+
+
 urlpatterns = [
+    path('search/', catalog, name='search'),
     path('<slug:category_slug>/', catalog, name='index'),
     path('product/<slug:product_slug>/', product, name='product'),
 ]
